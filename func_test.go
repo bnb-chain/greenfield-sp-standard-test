3b385a6965e5efa8b70fcba848c297cf1818a3ea
mocky-b <mock.y@binance.com>
2023-09-05 16:22:35 +0800

    1. support maintenance sp for test 2. add quota log for test

--- func_test.go
+++ func_test.go
@@ -68,7 +68,7 @@ func (s *SPFunctionalTestSuite) Test_01_UploadMultiSizeFile() {
 	bucketTx, err := testAccount.CreateBucket(bucketName, nil)
 	s.NoError(err)
 	log.Infof("Created bucket: %s, txHash: %s", bucketName, bucketTx)
-	bucket, err := testAccount.SDKClient.BuyQuotaForBucket(context.Background(), bucketName, 250*1024*1024, sdkTypes.BuyQuotaOption{})
+	bucket, err := testAccount.SDKClient.BuyQuotaForBucket(context.Background(), bucketName, 3*1024*1024*1024, sdkTypes.BuyQuotaOption{})
 	s.NoError(err, "BuyQuotaForBucket error")
 	log.Infof("BuyQuotaForBucket bucket: %s, txHash: %s", bucketName, bucket)
 	totalDownload := int64(0) //nolint:govet
